781ba79721c826281442687a1956e9de08de90fc
Austin Baxter <austinbaxter6202@outlook.com>
2021-06-20 01:11:32 -0700

    edited CSS and added javascript
    changed some colors of CSS and added in the javascript

---
+++ web_app_dashboard_v3.2/js/dashboard.js
@@ -0,0 +1,38 @@
+let bellSvg = document.getElementById('bellsvg');
+let trafficNav = document.querySelectorAll('.traffic-nav-link');
+let alertBanner = document.getElementById('alert');
+const trafficCanvas = document.querySelector('#traffic-chart');
+const doughnutCanvas = document.querySelector('#doughnut-chart');
+
+const user = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('sendBtn');
+
+
+alertBanner.innerHTML =
+`
+  <div class="alert-banner">
+  <p><strong>Alert:</strong> You have <strong>3</strong> overdue tasks to complete</p>
+  </div>
+  <div>
+  <p class="alert-banner-close">x</p>
+  </div>
+`
+  alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+      alertBanner.style.display = "none";
+    }
+  });
+
+send.addEventListener('click', () => {
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out both fields before attempting to send.")
+  } else if (user.value === "") {
+    alert("Please fill out user field before sending.")
+  } else if (message.value === "") {
+    alert("Please fill out message field before sending.")
+  } else {
+    alert(`Message was successfully sent to: ${user.value}`);
+  }
+});
---
+++ web_app_dashboard_v3.2/js/dropdown.js
@@ -0,0 +1,28 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
